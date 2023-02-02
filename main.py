bdb3b550a9b4f0ce0ae49d8f3141bbd0cc853a42
leandrofavila <leandrofavila@gmail.com>
2023-02-02 09:24:01 -0300

    trabalhando na função dispara_email

--- main.py
+++ main.py
@@ -1,66 +1,108 @@
-from flask import Flask, render_template, request, redirect, session, flash
+from flask import Flask, render_template, request, redirect, session, flash, url_for
+import cx_Oracle
 
 
 class Jogo:
-    def __init__(self, nome, categoria, console):
+    def __init__(self, ordem, dem_out, dem_in):
+        self.ordem = ordem
+        self.dem_out = dem_out
+        self.dem_in = dem_in
+
+
+class Usuario:
+    def __init__(self, nome, nickname, senha):
         self.nome = nome
-        self.categoria = categoria
-        self.console = console
+        self.nickname = nickname
+        self.senha = senha
 
 
 lista = []
 
+usuario1 = Usuario('KBCA', 'kbca', 'alo')
+usuario2 = Usuario('pana', 'pana', '123')
+usuario3 = Usuario('pançudo', 'barriga', '123')
+
+usuarios = {usuario1.nickname: usuario1,
+            usuario2.nickname: usuario2,
+            usuario3.nickname: usuario3
+            }
+
 app = Flask(__name__)
 
 app.secret_key = 'kbca'
+                   
+
 
 
 @app.route('/')
 def index():
-    return render_template('lista.html', titulo='Jogos', jogos=lista)
-
-
-@app.route('/novo')
-def novo():
-    return render_template('novo.html', titulo='Novo Jogo')
+    if 'usuario_logado' not in session or session['usuario_logado'] is None:
+        return redirect(url_for('login', proxima=url_for('index')))
+    return render_template('lista.html', titulo='Ordens', jogos=lista)
 
 
 @app.route('/criar', methods=['POST', ])
 def criar():
-    nome = request.form['nome']
-    categoria = request.form['categoria']
-    console = request.form['console']
-    jogo = Jogo(nome, categoria, console)
+    ordem = request.form['ordem']
+    dem_in = request.form['dem_in']
+    dem_out = focco(ordem)
+    jogo = Jogo(ordem, dem_out, dem_in)
     lista.append(jogo)
-    focco()
-    return redirect('/')
+    return redirect(url_for('index'))
 
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
 
 
 @app.route('/autenticar', methods=['POST', ])
 def autenticar():
-    if 'alo' == request.form['senha']:
-        session['usuario_logado'] = request.form['usuario']
-        flash('Usuario ' + session['usuario_logado'] + ' Logado com Sucesso!')
-        return redirect('/')
+    if request.form['usuario'] in usuarios:
+        usuario = usuarios[request.form['usuario']]
+        if request.form['senha'] == usuario.senha:
+            session['usuario_logado'] = usuario.nickname
+            flash(usuario.nickname + ' Logado com Sucesso!')
+            proxima_pagina = request.form['proxima']
+            return redirect(proxima_pagina)
     else:
         flash('Usuario não logado')
-        return redirect('/login')
+        return redirect(url_for('login'))
 
 
 @app.route('/logout')
 def logout():
     session['usuario_logado'] = None
     flash('Logout Efetuado')
-    return redirect('/')
+    return redirect(url_for('index'))
 
 
-def focco():
-    pass
+def focco(ordem):
+    dsn = cx_Oracle.makedsn("10.40.3.10", 1521, service_name="f3ipro")
+    connection = cx_Oracle.connect(user=r"focco_consulta", password=r'consulta3i08', dsn=dsn, encoding="UTF-8")
+    cur = connection.cursor()
+    cur.execute(
+        r"SELECT TPL.COD_ITEM  "
+        r"FROM FOCCO3I.TORDENS TOR "
+        r"INNER JOIN FOCCO3I.TDEMANDAS TDE                    ON TOR.ID = TDE.ORDEM_ID "
+        r"INNER JOIN FOCCO3I.TITENS_PLANEJAMENTO TPL          ON TDE.ITPL_ID = TPL.ID "
+        r"WHERE TOR.NUM_ORDEM IN (" + ordem + ") "
+    )
+    dem_out_focco = cur.fetchall()
+    print(type(dem_out_focco))
+    return dem_out_focco
 
 
-app.run(debug=True)
+@app.route('/dispara_email', methods=['POST', ])
+def dispara_email():
+    print('pana_dev')
+    lista.clear()
+    return redirect(url_for('index'))
+
+
+
+if __name__ == '__main__':
+    app.run(host='10.40.3.48', port=8010, debug=True)
+
+
