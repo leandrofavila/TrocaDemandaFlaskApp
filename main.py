b61ffc1eea00ea7b24452cd96bdc36d022b49a9a
leandrofavila <leandrofavila@gmail.com>
2023-02-03 14:39:04 -0300

    multiplas ordens por linha de input do html

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request, redirect, session, flash, url_for
 import cx_Oracle
+import pandas as pd
+import imaplib
+from email.message import Message
+from time import time
 
 
 class Jogo:
@@ -18,6 +22,7 @@ class Usuario:
 
 lista = []
 
+
 usuario1 = Usuario('KBCA', 'kbca', 'alo')
 usuario2 = Usuario('pana', 'pana', '123')
 usuario3 = Usuario('pançudo', 'barriga', '123')
@@ -30,8 +35,6 @@ usuarios = {usuario1.nickname: usuario1,
 app = Flask(__name__)
 
 app.secret_key = 'kbca'
-                   
-
 
 
 @app.route('/')
@@ -46,8 +49,10 @@ def criar():
     ordem = request.form['ordem']
     dem_in = request.form['dem_in']
     dem_out = focco(ordem)
-    jogo = Jogo(ordem, dem_out, dem_in)
-    lista.append(jogo)
+    ordens = ordem.split('\n')
+    for j in ordens:
+        jogo = Jogo(j, dem_out['num_ordem'][0] + ' - ' + dem_out['desc_tecnica'][0], dem_in + ' - ' + dem_out['desc_dem_in'][0])
+        lista.append(jogo)
     return redirect(url_for('index'))
 
 
@@ -82,27 +87,45 @@ def focco(ordem):
     dsn = cx_Oracle.makedsn("10.40.3.10", 1521, service_name="f3ipro")
     connection = cx_Oracle.connect(user=r"focco_consulta", password=r'consulta3i08', dsn=dsn, encoding="UTF-8")
     cur = connection.cursor()
+    ordem = ordem.split()
+    print(ordem)
+    s = ','.join(ordem)
+    print(s)
     cur.execute(
-        r"SELECT TPL.COD_ITEM  "
+        r"SELECT TPL.COD_ITEM, TIT.DESC_TECNICA, (SELECT TIT.DESC_TECNICA FROM FOCCO3I.TITENS TIT WHERE TIT.COD_ITEM IN (" + request.form['dem_in'] + "))  "
         r"FROM FOCCO3I.TORDENS TOR "
         r"INNER JOIN FOCCO3I.TDEMANDAS TDE                    ON TOR.ID = TDE.ORDEM_ID "
         r"INNER JOIN FOCCO3I.TITENS_PLANEJAMENTO TPL          ON TDE.ITPL_ID = TPL.ID "
-        r"WHERE TOR.NUM_ORDEM IN (" + ordem + ") "
+        r"INNER JOIN FOCCO3I.TITENS_EMPR EMP                  ON EMP.COD_ITEM = TPL.COD_ITEM "
+        r"INNER JOIN FOCCO3I.TITENS TIT                       ON TIT.ID = EMP.ITEM_ID "
+        r"WHERE TOR.NUM_ORDEM IN (" + s + ") "
     )
     dem_out_focco = cur.fetchall()
-    print(type(dem_out_focco))
+    dem_out_focco = pd.DataFrame(dem_out_focco, columns=['num_ordem', 'desc_tecnica', 'desc_dem_in'])
     return dem_out_focco
 
 
 @app.route('/dispara_email', methods=['POST', ])
 def dispara_email():
-    print('pana_dev')
+    connection = imaplib.IMAP4_SSL('10.40.3.12', 993)
+    connection.login("ldeavila@sr.ind.br", "srengld21v3l1")
+    new_message = Message()
+    new_message["From"] = "leandrofavila@gmail.com"
+    new_message["Subject"] = "Troca de demanda."
+    new_message.set_payload(trata_email())
+    connection.append('INBOX', '', imaplib.Time2Internaldate(time()), str(new_message).encode('utf-8'))
+    flash('Ordens enviadas para supervisor da produção')
     lista.clear()
     return redirect(url_for('index'))
 
 
+def trata_email():
+    nlis = ''
+    for x in lista:
+        nlis = nlis + x.ordem + ' ' + x.dem_in + ' ' + x.dem_out + '\n'
+
+    return nlis
+
 
 if __name__ == '__main__':
     app.run(host='10.40.3.48', port=8010, debug=True)
-
-
