fc95586d785207538307ff38111d3b65177f992c
leandrofavila <leandrofavila@gmail.com>
2023-07-03 07:37:50 -0300

    Add no bd PCP

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import datetime
+import pyodbc
+import re
 
 
 class Troca_dem:
@@ -25,13 +27,16 @@ class Usuario:
 
 lista_email = []
 lista = []
+
 usuario1 = Usuario('KBCA', 'kbca', 'alo')
 usuario2 = Usuario('pana', 'pana', '123')
 usuario3 = Usuario('barriga', 'barriga', '123')
+usuario4 = Usuario('amanda', 'amanda', 'qualidade01')
 
 usuarios = {usuario1.nickname: usuario1,
             usuario2.nickname: usuario2,
-            usuario3.nickname: usuario3
+            usuario3.nickname: usuario3,
+            usuario4.nickname: usuario4
             }
 
 app = Flask(__name__)
@@ -51,7 +56,6 @@ def criar():
     ordem = request.form['ordem']
     dem_in = request.form['dem_in']
     dem_out = focco(ordem)
-    #print(dem_out.to_string())
     for j in dem_out.iterrows():
         jogo = Troca_dem(j[1][0], j[1][1], j[1][2], j[1][3] + ' - ' + j[1][4], dem_in + ' - ' + j[1][6])
         lista.append(jogo)
@@ -70,7 +74,7 @@ def autenticar():
         usuario = usuarios[request.form['usuario']]
         if request.form['senha'] == usuario.senha:
             session['usuario_logado'] = usuario.nickname
-            flash(usuario.nickname + ' Logado com Sucesso!')
+            flash(usuario.nickname + ' logado com Sucesso!')
             proxima_pagina = request.form['proxima']
             return redirect(proxima_pagina)
     else:
@@ -82,6 +86,7 @@ def autenticar():
 def logout():
     session['usuario_logado'] = None
     flash('Logout Efetuado')
+    lista.clear()
     return redirect(url_for('index'))
 
 
@@ -105,7 +110,6 @@ def focco(ordem):
                                           r"AND TIT.DESC_TECNICA NOT LIKE '%TINTA%' "
     )
     dem_out_focco = cur.fetchall()
-
     connection.commit()
     cur.execute(
         r"SELECT TIT.COD_ITEM, TIT.DESC_TECNICA "
@@ -114,13 +118,13 @@ def focco(ordem):
     )
     dem_out_focco_dem_in = cur.fetchall()
     dem_out_focco_dem_in = pd.DataFrame(dem_out_focco_dem_in, columns=['dem_in', 'desc_dem_in'])
-
     dem_out_focco = pd.DataFrame(dem_out_focco, columns=['num_ordem',
                                                          'cod_item',
                                                          'desc_ordem',
                                                          'num_dem',
                                                          'desc_tecnica'
                                                          ])
+
     dem_out_focco['dem_in'] = dem_out_focco_dem_in['dem_in'][0]
     dem_out_focco['desc_dem_in'] = dem_out_focco_dem_in['desc_dem_in'][0]
     return dem_out_focco
@@ -128,11 +132,13 @@ def focco(ordem):
 
 @app.route('/dispara_email', methods=['POST', ])
 def dispara_email():
+    add_bd()
     msg = MIMEMultipart()
     message = trata_email()
     password = "srengld21v3l1"
     msg['From'] = "ldeavila@sr.ind.br"
-    recipients = ["ldeavila@sr.ind.br"]  # "producao@sr.ind.br", "wesley@sr.ind.br"
+    recipients = [
+        "ldeavila@sr.ind.br", "producao@sr.ind.br", "qualidade@sr.ind.br"]  # "producao@sr.ind.br", "wesley@sr.ind.br"
     msg['To'] = ", ".join(recipients)
     msg['Subject'] = "Troca de Demanda"
     msg.attach(MIMEText(message, 'plain'))
@@ -141,25 +147,25 @@ def dispara_email():
     server.login(msg['From'], password)
     server.sendmail(msg['From'], recipients, msg.as_string())
     server.quit()
-    flash('Ordens enviadas para supervisor da produção')
+    flash('Ordens enviadas para o(a) supervisor(a) avaliar')
     lista.clear()
     return redirect(url_for('index'))
 
 
 def trata_email():
     df = pd.DataFrame([vars(d) for d in lista])
-    #print(df.to_string())
     df_uniq = df['dem_in'].unique()
-    nlis = sauda() + '\nFavor trocar as demandas das ordens relacionadas abaixo:\n \n'
+    nlis = sauda() + '\nFavor trocar a(s) demanda(s) da(s) orden(s) relacionada(s) abaixo:\n \n'
     for x in df_uniq:
         condi = df.loc[df['dem_in'] == x, 'dem_out'].iloc[0]
-        #print(condi)
-        nlis += 'Sai a demanda: ' + condi + '\n'\
-               + 'Entra a demanda: ' + x + '\n'
+        nlis += 'Sai a demanda: ' + condi + '\n' \
+                + 'Entra a demanda: ' + x + '\n \n'
         for idx, vals in df.iterrows():
             if vals['dem_in'] == x:
-                nlis += '\t' + '•' + str(vals['ordem']) + ' - ' + str(vals['cod_item']) + ' - ' + str(vals['desc_item']) + '\n'
-        nlis += '\n \n ____________________________________________________________________________________ \n \n'
+                nlis += '\t' + '•' + str(vals['ordem']) + ' - ' + str(vals['cod_item']) + ' - ' + str(
+                    vals['desc_item']) + '\n'
+        nlis += '\n ___________________________________________________________________________________\n \n'
+    nlis += '\n \nQualquer duvida estou a disposição.\nAtt.'
     return nlis
 
 
@@ -171,6 +177,24 @@ def sauda():
         return 'Boa tarde: '
     else:
         return 'Boa noite: '
+
+
+def add_bd():
+    df = pd.DataFrame([vars(d) for d in lista])
+    conn = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
+                          "Server=ALFA\SQLEXPRESS;"
+                          "Database=ReaData;"
+                          "UID=PCPUser;"
+                          "PWD=*$rUserPcP;")
+    cursor = conn.cursor()
+    for item in df.index:
+        #print(df['cod_item'][item])
+        dem_out = re.findall('([^\s]+)', df['dem_out'][item])
+        dem_in = re.findall('([^\s]+)', df['dem_in'][item])
+        cursor.execute(r"insert into [ReaData].[dbo].[tTrocaDem]"
+                       r"(ORDEM, DEM_OUT, DEM_IN, DT_IN) "
+                       r"values(" + str(df['ordem'][item]) + ", " + str(dem_out[0]) + ", " + str(dem_in[0]) + ", getdate()) ")
+        conn.commit()
 
 
 if __name__ == '__main__':
